24242cb39850c9f4df9a545dc106f1b966eedcea
nayeong01 <108383043+nayeong01@users.noreply.github.com>
2022-09-18 17:07:54 +0900

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,21 @@
+package codingTest;
+
+import java.util.*;
+
+class test1 {
+    public String solution(String[] participant, String[] completion) {
+    	
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        int i;
+        
+        for (i=0; i<completion.length; i++) {
+
+            if (!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+        return participant[i];
+    }
+}
